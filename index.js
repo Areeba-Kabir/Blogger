d89ec6ab3f1caf785b42bdeec0fa3031308dc793
Areeeba <areeebakabir137@gmail.com>
2024-05-23 17:21:54 +0500

    Blogger 4th commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
 const connection = require('./connection');
+const blogroute = require('./Router/blogroutes.js');
+const router = require('./Router/blogroutes.js');
+
+const app = express();
 app.use(express.json());
 
+app.use('/api/blog', blogroute);
+
 connection().then(
-    () => {
+    (res) => {
         console.log('Connected to database');
         const port = process.env.PORT || 3000;
         app.listen(port, () => {
             console.log(`server listening on PORT: ${port}`)
         })
     }
-).catch(()=>{console.log('error connecting to db')})
+).catch((err) => { console.log('error connecting to db') })
+
