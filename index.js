bdc4005b1b75412e85a10d937d8f4af536b99c4b
Areeeba <areeebakabir137@gmail.com>
2024-05-24 15:09:37 +0500

    Blogger 8th commit --> removal of url shortner implementation model-controller-routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connection = require('./connection');
 const blogroute = require('./Router/blogroutes.js');
-
 const bloglogs = require('./Middleware/bloglog.js');
 const app = express();
 
@@ -15,7 +14,6 @@ const app = express();
 
 
 app.use(express.json());
-app.set("view engine","ejs")
 app.use(bloglogs('logs.txt'));
 app.use('/api/blog', blogroute);
 app.use(express.urlencoded({ extended: false }));
